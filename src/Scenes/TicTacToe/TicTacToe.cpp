418076d7d9980832f2da98f0a6a0b93a2d4c12aa
Ashley Smith <ashley@aas.sh>
2019-09-15 19:19:09 +0100

    Game state can be rendered for tic-tac-toe

--- src/Scenes/TicTacToe/TicTacToe.cpp
+++ src/Scenes/TicTacToe/TicTacToe.cpp
@@ -27,6 +27,43 @@ TicTacToeScene::onRender(sf::RenderWindow& window) {
 
   // Draw the background first
   window.draw(board_);
+
+  // Draw the state of the game
+  // @TODO: Load the gamestate rather than making the fresh on
+  drawGameState(window, GameState());
+}
+
+// Draw the given state
+void
+TicTacToeScene::drawGameState(
+    sf::RenderWindow& window, 
+    const GameState& state) {
+
+  // For each tile of the board
+  for (int j = 0; j < BOARDSIZE; ++j) {
+    for (int i = 0; i < BOARDSIZE; ++i) {
+
+      // Grab the player who owns the tile
+      const Player tile = state.boardState[j][i];
+      const auto pos = sf::Vector2f(
+          left_ + i * tileSize_, 
+          top_ + j * tileSize_);
+      
+      // Only act if a player owns the tile
+      if (tile == Player::X) {
+        auto cross = playerIconX_;
+        for (int k = 0; k < 4; ++k) {
+          cross[k].position += pos;
+        }
+        window.draw(cross);
+      }
+      else if (tile == Player::O) {
+        auto circle = playerIconO_;
+        circle.setPosition(pos + sf::Vector2f(tileSize_, tileSize_) * 0.5f);
+        window.draw(circle);
+      }
+    }
+  }
 }
 
 // Adjust graphics for current game size
@@ -37,38 +74,54 @@ TicTacToeScene::resizeGame() {
   const auto displaySize = App::getDisplaySize();
 
   // Get square size based on smallest dimension of display
-  const float gameSize = 
+  gameSize_ = 
       (displaySize.x < displaySize.y ? 
       displaySize.x : displaySize.y) * 0.6f;
-  const float tileSize = gameSize / 3.f;
+  tileSize_ = gameSize_ / (float) BOARDSIZE;
 
   // Get positions
-  const auto center = displaySize * 0.5f;
-  const float left = center.x - (gameSize * 0.5f);
-  const float top = center.y - (gameSize * 0.5f);
-  const float right = left + gameSize;
-  const float bottom = top + gameSize;
+  center_ = displaySize * 0.5f;
+  left_ = center_.x - (gameSize_ * 0.5f);
+  top_ = center_.y - (gameSize_ * 0.5f);
+  right_= left_ + gameSize_;
+  bottom_ = top_ + gameSize_;
 
-  // Create a new board
+  // Create player symbols
+  // Player X
+  playerIconX_ = sf::VertexArray(sf::Lines, 4);
+  playerIconX_[0].position = sf::Vector2f(tileSize_ * 0.2f, tileSize_ * 0.2f);
+  playerIconX_[0].color = playerXColour_;
+  playerIconX_[1].position = sf::Vector2f(tileSize_ * 0.8f, tileSize_ * 0.8f);
+  playerIconX_[1].color = playerXColour_;
+  playerIconX_[2].position = sf::Vector2f(tileSize_ * 0.8f, tileSize_ * 0.2f);
+  playerIconX_[2].color = playerXColour_;
+  playerIconX_[3].position = sf::Vector2f(tileSize_ * 0.2f, tileSize_ * 0.8f);
+  playerIconX_[3].color = playerXColour_;
+  // Player O
+  playerIconO_ = sf::CircleShape(tileSize_ * 0.35f);
+  playerIconO_.setOrigin(tileSize_ * 0.35f, tileSize_ * 0.35f);
+  playerIconO_.setOutlineThickness(1.f);
+  playerIconO_.setOutlineColor(playerOColour_);
+  playerIconO_.setFillColor(sf::Color::Transparent);
+
+  // Create a board to draw
   board_ = sf::VertexArray(sf::Lines, 16);
-
   // Horizontal lines
-  board_[0].position = sf::Vector2f(left, top);
-  board_[1].position = sf::Vector2f(right, top);
-  board_[2].position = sf::Vector2f(left, top + tileSize);
-  board_[3].position = sf::Vector2f(right, top + tileSize);
-  board_[4].position = sf::Vector2f(left, top + tileSize * 2);
-  board_[5].position = sf::Vector2f(right, top + tileSize * 2);
-  board_[6].position = sf::Vector2f(left, bottom);
-  board_[7].position = sf::Vector2f(right, bottom);
-
+  board_[0].position = sf::Vector2f(left_, top_);
+  board_[1].position = sf::Vector2f(right_, top_);
+  board_[2].position = sf::Vector2f(left_, top_ + tileSize_);
+  board_[3].position = sf::Vector2f(right_, top_ + tileSize_);
+  board_[4].position = sf::Vector2f(left_, top_ + tileSize_ * 2);
+  board_[5].position = sf::Vector2f(right_, top_ + tileSize_ * 2);
+  board_[6].position = sf::Vector2f(left_, bottom_);
+  board_[7].position = sf::Vector2f(right_, bottom_);
   // Vertical lines
-  board_[8].position = sf::Vector2f(left, top);
-  board_[9].position = sf::Vector2f(left, bottom);
-  board_[10].position = sf::Vector2f(left + tileSize, top);
-  board_[11].position = sf::Vector2f(left + tileSize, bottom);
-  board_[12].position = sf::Vector2f(left + tileSize * 2, top);
-  board_[13].position = sf::Vector2f(left + tileSize * 2, bottom);
-  board_[14].position = sf::Vector2f(right, top);
-  board_[15].position = sf::Vector2f(right, bottom);
+  board_[8].position = sf::Vector2f(left_, top_);
+  board_[9].position = sf::Vector2f(left_, bottom_);
+  board_[10].position = sf::Vector2f(left_ + tileSize_, top_);
+  board_[11].position = sf::Vector2f(left_ + tileSize_, bottom_);
+  board_[12].position = sf::Vector2f(left_ + tileSize_ * 2, top_);
+  board_[13].position = sf::Vector2f(left_ + tileSize_ * 2, bottom_);
+  board_[14].position = sf::Vector2f(right_, top_);
+  board_[15].position = sf::Vector2f(right_, bottom_);
 }
--- src/Scenes/TicTacToe/TicTacToe.h
+++ src/Scenes/TicTacToe/TicTacToe.h
@@ -6,6 +6,29 @@
 
 #include "../../Scene.h"
 
+#define BOARDSIZE 3
+
+// Representation of players
+enum Player {
+  N,
+  X,
+  O
+};
+
+// Simple gamestate structure
+struct GameState {
+
+  // Track who's turn it is
+  Player currentTurn = Player::X;
+
+  // State of the board
+  Player boardState[BOARDSIZE][BOARDSIZE] = {
+    { N, N, O },
+    { X, X, O },
+    { N, N, O }
+  };
+};
+
 // Basic game of tic-tac-toe
 class TicTacToeScene : public Scene {
   public:
@@ -21,11 +44,30 @@ class TicTacToeScene : public Scene {
 
   private:
 
+    // Player colours
+    sf::Color playerXColour_ = sf::Color::Blue;
+    sf::Color playerOColour_ = sf::Color::Red;
+
+    // Player icons
+    sf::VertexArray playerIconX_;
+    sf::CircleShape playerIconO_;
+
     // The tic-tac-toe board
     sf::VertexArray board_;
 
+    // Key dimensions
+    float gameSize_;
+    float tileSize_;
+
+    // Key Positions
+    sf::Vector2f center_;
+    float top_, left_, right_, bottom_;
+
     // Adjust graphics for current game size
     void resizeGame();
+
+    // Draw a specific gamestate
+    void drawGameState(sf::RenderWindow& window, const GameState& state);
 };
 
 #endif
